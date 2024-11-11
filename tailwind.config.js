2ee809dd649daeb31a8ace67f3d9154ee2e57ea8
Temur Coder <temurnarzullayev810@gmail.com>
2024-11-11 17:48:53 +0300

    codeni yarmi bitdi

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,23 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        "green_bg": "#43A548",
-        "body_bg" : "#F7F8F8"
+        green_bg: "#43A548",
+        body_bg: "#F7F8F8",
+        grey_gb: "#EBF5EB",
       },
-      textColor:{
-        'black-text': "#2C2B35",
+      textColor: {
+        green_text: "#43A548",
+        'grey-text': "#6F7973",
+        "black-text": "#2C2B35",
       },
-      boxGradient:{
-        'btn-gradient': 'linear-gradient(100.98deg, #43A548 -4.14%, #52E259 108.69%);'
+      boxGradient: {
+        "btn-gradient":
+          "linear-gradient(100.98deg, #43A548 -4.14%, #52E259 108.69%);",
       },
+      boxShadow: {
+        "box-shadow":"0px 0px 10px 3px rgba(0,0,0,0.1)",
+        "box-shadow-hover":"0px 0px 10px 3px rgba(0,0,0,0.2)",
+      }
     },
     plugins: [],
   },
