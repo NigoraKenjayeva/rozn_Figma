61274ce728bf5efe53bdbe82f6026a1feb6bc299
Temur Coder <temurnarzullayev810@gmail.com>
2024-11-11 14:27:29 +0300

    accordion qoshdm

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       backgroundColor: {
         green_bg: "#43A548",
       },
+      textColor:{
+        'black-text': "#2C2B35",
+      },
       boxGradient:{
         'btn-gradient': 'linear-gradient(100.98deg, #43A548 -4.14%, #52E259 108.69%);'
       },
