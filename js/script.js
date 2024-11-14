442635cf44f39bfda037fba107d00155ddde7c27
Temur Coder <temurnarzullayev810@gmail.com>
2024-11-14 17:44:11 +0300

    navbarga scroll qoshdm

--- js/script.js
+++ js/script.js
@@ -42,3 +42,13 @@ form.addEventListener("submit", (e) => {
     }
   }
 });
+
+// Navbar scroll
+
+const scrollHeader = () =>{
+  const header = document.querySelector('.header')
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50 ? header.classList.add('bg-header') 
+                     : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
