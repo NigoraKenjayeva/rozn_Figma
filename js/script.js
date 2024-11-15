da4c48549d0563dce009d10d2dc1ef6646aa26d7
Temur Coder <temurnarzullayev810@gmail.com>
2024-11-15 14:03:53 +0300

    header class added

--- js/script.js
+++ js/script.js
@@ -9,41 +9,6 @@ menuToggle.addEventListener("click", () => {
   mobileMenu.classList.add("active-position");
 });
 
-// RegEx 
-
-const form = document.getElementById("form");
-const userName = document.getElementById("name");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-let inputT = document.querySelectorAll(".inp");
-let submitBtn = document.querySelector(".submit_btn");
-
-submitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const emailRegEx = /[a-z0-9]{6,12}@gmail/;
-  const phoneRegEx = /^[0-9]{8,12}$/;
-  const nameRegEx = /[a-zA-Z]/;
-  let emailValue = email.value;
-  let phoneValue = phone.value;
-  let nameValue = userName.value;
-  if (userName.value == "" || email.value == "" || phone.value == "") {
-    alert("Malumotlar To'liq emas.");
-  } else {
-    if (
-      emailRegEx.test(emailValue) &&
-      phoneRegEx.test(phoneValue) &&
-      nameRegEx.test(nameValue)
-    ) {
-      alert("Malumotlar qabul qilindi😊. Malumot uchun rahmat.👌");
-      userName.value = "";
-      email.value = "";
-      phone.value = "";
-    } else {
-      alert("Malumotlar qabul qilinmadi😒");
-    }
-  }
-});
-
 // Navbar scroll
 
 const scrollHeader = () => {
